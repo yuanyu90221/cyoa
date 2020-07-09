27737208866e040180ab18091baedb84e8f8fafd
jsonLiang <yuanyu90221@gmail.com>
2020-07-09 16:46:29 +0800

    add custom prefix and mux handler

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/yuanyu90221/cyoa"
 )
@@ -27,7 +29,84 @@ func main() {
 	}
 
 	// fmt.Printf("%+v\n", story)
-	h := cyoa.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+	h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl), cyoa.WithPathFunc(pathFn))
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
 	fmt.Printf("Starting the server at: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+
+	if path == "/story" || path == "/story/" { // default intro
+		path = "/story/intro"
+	}
+	// /intro -> intro
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Choose Your Own Adventure</title>
+	</head>
+	
+	<body>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+			<p>{{.}}</p>
+			{{end}}
+			<ul>
+				{{range .Options}}
+					<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+				{{end}}
+			</ul>
+		</section>
+		<style>
+			body {
+				font-family: helvetica, arial;
+			}
+			h1 {
+				text-align: center;
+				position:relative;
+			}
+			.page {
+				width: 80%;
+				max-width: 500px;
+				margin: auto;
+				margin-top: 40px;
+				margin-bottom: 40px;
+				padding: 80px;
+				background: #FFFCF6;
+				border: 1px solid #eee;
+				box-shadow: 0 10px 6px -6px #777;
+			}
+			ul {
+				border-top: 1px dotted #ccc;
+				padding: 10px 0 0 0;
+				-webkit-padding-start: 0;
+			}
+			li {
+				padding-top: 10px;
+			}
+			a,
+			a: visited {
+				text-decoration: none;
+				color: #6295b5;
+			}
+			a: active,
+			a: hover {
+				color: #7792a2;
+			}
+			p {
+				text-indent: 1em;
+			}
+		</style>
+    </body>
+</html>`
--- story.go
+++ story.go
@@ -86,8 +86,13 @@ func WithTemplate(t *template.Template) HandlerOption {
 	}
 }
 
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
-	h := handler{s, tpl}
+	h := handler{s, tpl, defaultPathFn}
 	for _, opt := range opts {
 		opt(&h)
 	}
@@ -95,18 +100,23 @@ func NewHandler(s Story, opts ...HandlerOption) http.Handler {
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 
 	if path == "" || path == "/" { // default intro
 		path = "/intro"
 	}
 	// /intro -> intro
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
 		err := h.t.Execute(w, chapter)
